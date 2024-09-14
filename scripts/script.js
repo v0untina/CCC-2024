458d61ea816e9c2900195864e4b4d9640121fc29
Sasha Surovtsev <neon4ik378@gmail.com>
2024-09-14 21:24:34 +0300

    need debug and MOREE css and text

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let canvas = document.getElementById("imageCanvas");
-  let ctx = canvas.getContext("2d");
-  let hiddenCanvas = document.createElement("canvas");
-  let hiddenCtx = hiddenCanvas.getContext("2d");
-  let image = new Image();
+  const canvas = document.getElementById("imageCanvas");
+  const ctx = canvas.getContext("2d");
+  const image = new Image();
   let originalImageData = null;
   let history = [];
   let historyStep = -1;
@@ -18,6 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
   let currentAngle = 0;
   let isFlippedHorizontally = false;
   let isFlippedVertically = false;
+  let imageX = 0, imageY = 0;
+
+
   const activeIcons = {
     crop_button: '/images/crop-active.png',
     resize_button: '/images/resize-active.png',
@@ -48,20 +49,16 @@ document.addEventListener('DOMContentLoaded', function () {
         let scale = Math.min(MAX_WIDTH / image.width, MAX_HEIGHT / image.height);
         let newWidth = image.width * scale;
         let newHeight = image.height * scale;
-  
+
         canvas.width = newWidth;
         canvas.height = newHeight;
-        hiddenCanvas.width = newWidth;
-        hiddenCanvas.height = newHeight;
-  
+
         ctx.drawImage(image, 0, 0, newWidth, newHeight);
-        hiddenCtx.drawImage(image, 0, 0, newWidth, newHeight);
-  
         originalImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         canvas.classList.add('canvas-border');
         canvas.style.display = 'block';
         document.getElementById('dropArea').classList.add('hidden');
-        saveState();
+        saveState(); // Save initial state
       };
     };
     reader.readAsDataURL(file);
@@ -108,17 +105,30 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("dropArea").addEventListener("dragover", handleDragOver);
 
   function saveState() {
+    let imageData = canvas.toDataURL();
+    let state = {
+        image: imageData,
+        figures: figures,
+        filters: ctx.filter,
+        position: { x: imageX, y: imageY }, // Положение изображения
+        angle: currentAngle,
+        isFlippedHorizontally: isFlippedHorizontally,
+        isFlippedVertically: isFlippedVertically
+    };
     history = history.slice(0, historyStep + 1);
-    history.push(canvas.toDataURL());
-    historyStep++;
-  }
+    history.push(state);
+    historyStep = history.length - 1;
+}
 
-  function restoreState(dataUrl) {
+  
+  function restoreState(state) {
     let img = new Image();
-    img.src = dataUrl;
+    img.src = state.image;
     img.onload = function () {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.drawImage(img, 0, 0);
+      figures = state.figures;
+      drawFigures();
     };
   }
   
@@ -141,38 +151,35 @@ document.addEventListener('DOMContentLoaded', function () {
   function applyImageFilter(filterType) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.filter = getFilterCSS(filterType);
-    ctx.drawImage(hiddenCanvas, 0, 0);
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    drawFigures();
     saveState();
   }
 
   function getFilterCSS(filterType) {
-      switch (filterType) {
-          case 'none':
-              return 'none';
-          case 'sepia':
-              return 'sepia(100%)';
-          case 'grayscale':
-              return 'grayscale(100%)';
-          case 'vintage':
-              return 'sepia(50%) contrast(85%) saturate(120%)';
-          default:
-              return 'none';
-      }
+    switch (filterType) {
+      case 'none':
+        return 'none';
+      case 'sepia':
+        return 'sepia(100%)';
+      case 'grayscale':
+        return 'grayscale(100%)';
+      case 'vintage':
+        return 'sepia(50%) contrast(85%) saturate(120%)';
+      default:
+        return 'none';
+    }
   }
 
   document.getElementById("undoButton").addEventListener("click", function () {
     if (historyStep > 0) {
-        historyStep--;
-        restoreState(history[historyStep]);
+      historyStep--;
+      restoreState(history[historyStep]);
 
-        if (lastSlider) {
-            document.getElementById(lastSlider).value = 100; 
-            document.getElementById("brightness").value = 100;
-            document.getElementById("contrast").value = 100;
-            document.getElementById("saturation").value = 100;
-            document.getElementById("exposure").value = 100;
-            applyFilters();
-        }
+      if (lastSlider) {
+        document.getElementById(lastSlider).value = 100;
+        applyFilters();
+      }
     }
   });
 
@@ -181,9 +188,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("saturation").addEventListener("input", applyFilters);
   document.getElementById("exposure").addEventListener("input", applyFilters);
 
-
-  
-function applyFilters() {
+  function applyFilters() {
     const brightness = document.getElementById("brightness").value;
     const contrast = document.getElementById("contrast").value;
     const saturation = document.getElementById("saturation").value;
@@ -193,23 +198,18 @@ function applyFilters() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Применяем фильтры
+    // Применение фильтров
     ctx.filter = `brightness(${brightness * exposureEffect}%) contrast(${contrast}%) saturate(${saturation}%)`;
-
-    // Применяем текущие трансформации поворота и переворота
     ctx.save();
     ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate((currentAngle * Math.PI) / 180);
-    const scaleX = isFlippedHorizontally ? -1 : 1;
-    const scaleY = isFlippedVertically ? -1 : 1;
-    ctx.scale(scaleX, scaleY);
-
-    // Рисуем изображение с учётом трансформаций
-    ctx.drawImage(hiddenCanvas, -canvas.width / 2, -canvas.height / 2);
+    ctx.rotate(currentAngle * Math.PI / 180);
+    ctx.scale(isFlippedHorizontally ? -1 : 1, isFlippedVertically ? -1 : 1);
+    ctx.drawImage(image, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
     ctx.restore();
-
+    drawFigures();
     saveState();
 }
+
 
 
   document.getElementById("redoButton").addEventListener("click", function () {
@@ -251,27 +251,29 @@ function applyFilters() {
 
   function rotateImage(angle) {
     currentAngle = (currentAngle + angle) % 360;
+    const rad = (currentAngle * Math.PI) / 180;
+    const scaleX = isFlippedHorizontally ? -1 : 1;
+    const scaleY = isFlippedVertically ? -1 : 1;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate((currentAngle * Math.PI) / 180);
-    const scaleX = isFlippedHorizontally ? -1 : 1;
-    const scaleY = isFlippedVertically ? -1 : 1;
+    ctx.rotate(rad);
     ctx.scale(scaleX, scaleY);
-    ctx.drawImage(hiddenCanvas, -canvas.width / 2, -canvas.height / 2);
+    ctx.drawImage(image, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
     ctx.restore();
     saveState();
 }
 
-
-  function flipImage(direction) {
+function flipImage(direction) {
     if (direction === 'horizontal') {
-      isFlippedHorizontally = !isFlippedHorizontally;
+        isFlippedHorizontally = !isFlippedHorizontally;
     } else if (direction === 'vertical') {
-      isFlippedVertically = !isFlippedVertically;
+        isFlippedVertically = !isFlippedVertically;
     }
-    rotateImage(0);
-  }
+    rotateImage(0); // Перерисовываем с учетом новых настроек отражения
+}
+
 
   document.querySelector('.rotate-left').addEventListener('click', function () {
     rotateImage(-90);
@@ -397,14 +399,53 @@ function applyFilters() {
     saveState();
   });
 
-  document.getElementById("resizeButton").addEventListener("click", function () {
+  function resizeImage(newWidth, newHeight) {
+    // Сохранение текущих данных изображения, фильтров, положения, и состояния
+    let imgData = canvas.toDataURL();
+    let currentState = {
+        image: imgData,
+        figures: figures,
+        filters: ctx.filter,
+        position: { x: imageX, y: imageY }, // Положение изображения
+        angle: currentAngle,
+        isFlippedHorizontally: isFlippedHorizontally,
+        isFlippedVertically: isFlippedVertically
+    };
+
+    // Перерисовка изображения с новым размером
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let img = new Image();
+    img.src = imgData;
+    img.onload = function() {
+        // Применение фильтров
+        ctx.filter = currentState.filters;
+        
+        // Восстановление положения и состояния поворота и отражения
+        ctx.save();
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(currentState.angle * Math.PI / 180);
+        ctx.scale(currentState.isFlippedHorizontally ? -1 : 1, currentState.isFlippedVertically ? -1 : 1);
+        ctx.drawImage(img, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+        ctx.restore();
+
+        // Восстановление и отрисовка фигур
+        figures = currentState.figures; // Восстанавливаем фигуры
+        drawFigures();
+        saveState(); // Сохраняем новое состояние
+    };
+}
+
+
+document.getElementById("resizeButton").addEventListener("click", function () {
     let newWidth = document.getElementById("resizeWidth").value;
     let newHeight = document.getElementById("resizeHeight").value;
     let keepAspectRatio = document.getElementById("keepAspectRatio").checked;
 
     newWidth = parseInt(newWidth);
     newHeight = parseInt(newHeight);
-
     if (newWidth > MAX_WIDTH) {
         newWidth = MAX_WIDTH;
     }
@@ -413,7 +454,7 @@ function applyFilters() {
     }
 
     if (keepAspectRatio) {
-        let aspectRatio = image.width / image.height;
+        let aspectRatio = canvas.width / canvas.height;
         if (newWidth / newHeight > aspectRatio) {
             newWidth = newHeight * aspectRatio;
         } else {
@@ -421,44 +462,11 @@ function applyFilters() {
         }
     }
 
-    let previousFilters = ctx.filter; // Сохраняем текущее состояние фильтров
-
-    // Устанавливаем новый размер холста
-    canvas.width = newWidth;
-    canvas.height = newHeight;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    let x = 0;
-    let y = 0;
-    let width = newWidth;
-    let height = newHeight;
-
-    if (width > canvas.width) {
-        width = canvas.width;
-        height = (width / newWidth) * newHeight;
-    }
-
-    if (height > canvas.height) {
-        height = canvas.height;
-        width = (height / newHeight) * newWidth;
-    }
-
-    x = (canvas.width - width) / 2;
-    y = (canvas.height - height) / 2;
-
-    // Рисуем изображение с новым размером
-    ctx.drawImage(image, x, y, width, height);
-    hiddenCanvas.width = newWidth;
-    hiddenCanvas.height = newHeight;
-    hiddenCtx.drawImage(image, x, y, width, height);
-
-    // Восстанавливаем фильтры
-    ctx.filter = previousFilters;
-
-    saveState();
+    resizeImage(newWidth, newHeight);
 });
-
   
+
+
   function getClickedFigure(x, y) {
     for (let i = figures.length - 1; i >= 0; i--) {
       let figure = figures[i];
